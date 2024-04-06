ff5a01094685bff61d29421547041e465823b66f
Sultan Mukhtarov <suli419956@gmail.com>
2024-04-06 16:35:04 +0600

    created base setting for store and added theme.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {CssBaseline, ThemeProvider} from '@mui/material';
+import theme from './theme.ts';
+import {Provider} from 'react-redux';
+import {store} from './app/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>,
+  </Provider>
+
 )
